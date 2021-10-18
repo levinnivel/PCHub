d1c05d2798787526b8e12163ebe0ccb11e7919e7
andreasVi <andreasvirdian07@gmail.com>
2021-10-18 19:54:54 +0700

    generate code

---
+++ app/Models/Shipment.php
@@ -0,0 +1,25 @@
+<?php
+require_once ('DetailOrder.php');
+
+class Shipment
+{
+
+	private $address;
+	private $arrival_date;
+	private $id;
+	private $ship_date;
+	private $type;
+	private $m_DetailOrder;
+
+	function __construct()
+	{
+	}
+
+	function __destruct()
+	{
+	}
+
+
+
+}
+?>
